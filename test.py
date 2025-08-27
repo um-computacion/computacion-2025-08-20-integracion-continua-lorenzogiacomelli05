e21f4db3a9ea770f7ba5740c0b78bc9e29d8650f
Lorenzo Giacomelli <lorenzogiacomelli@gmail.com>
2025-08-27 08:30:42 -0300

    Agregado de la funcion resta

--- test.py
+++ test.py
@@ -6,5 +6,8 @@ class TestPrueba(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(suma(2, 3), 5)
 
+    def test_resta(self):
+        self.assertEqual(suma(5, -3), 2)
+
 if __name__ == "__main__":
     print(suma(2, 3))
